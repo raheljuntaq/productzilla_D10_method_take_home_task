3a3e8e030c13c5ff3c0ab82c0fa1a7b9cf1f4414
raheljuntaq <rahel.anastasyapithaloka2086@students.unila.ac.id>
2023-03-14 11:32:55 +0700

    first commit

---
+++ public/script1.js
@@ -0,0 +1,219 @@
+// 1
+let umur;
+function setUmur(argUmur) {
+  umur = argUmur;
+}
+
+function getUmur() {
+  return umur;
+}
+console.log(getUmur());
+
+// 2
+let alamat;
+function setAlamat(argAlamat) {
+  alamat = argAlamat;
+}
+
+function getAlamat() {
+  return alamat;
+}
+console.log(getAlamat());
+
+// 3
+let pekerjaan;
+function setPekerjaan(argPekerjaan) {
+  pekerjaan = argPekerjaan;
+}
+
+function getPekerjaan() {
+  return pekerjaan;
+}
+console.log(getPekerjaan());
+
+// 4
+let gaji;
+function setGaji(argGaji) {
+  gaji = argGaji;
+}
+
+function getGaji() {
+  return gaji;
+}
+console.log(getGaji());
+
+// 5
+let hobi;
+function setHobi(argHobi) {
+  hobi = argHobi;
+}
+
+function getHobi() {
+  return hobi;
+}
+console.log(getHobi());
+
+// 6
+let email;
+function setEmail(argEmail) {
+  email = argEmail;
+}
+
+function getEmail() {
+  return email;
+}
+console.log(getEmail());
+
+// 7
+let tglLahir;
+function setTglLahir(argTglLahir) {
+  tglLahir = argTglLahir;
+}
+
+function getTglLahir() {
+  return tglLahir;
+}
+console.log(getTglLahir());
+
+// 8
+let tempatLahir;
+function setTempatLahir(argTempatLahir) {
+  tempatLahir = argTempatLahir;
+}
+
+function getTempatLahir() {
+  return tempatLahir;
+}
+console.log(getTempatLahir());
+
+// 9
+let pendidikan;
+function setPendidikan(argPendidikan) {
+  pendidikan = argPendidikan;
+}
+
+function getPendidikan() {
+  return pendidikan;
+}
+console.log(getPendidikan());
+
+// 10
+let universitas;
+function setUniversitas(argUniversitas) {
+  universitas = argUniversitas;
+}
+
+function getUniversitas() {
+  return universitas;
+}
+console.log(getUniversitas());
+
+// 11
+let statusPernikahan;
+function setStatusPernikahan(argStatusPernikahan) {
+  statusPernikahan = argStatusPernikahan;
+}
+
+function getStatusPernikahan() {
+  return statusPernikahan;
+}
+console.log(getStatusPernikahan());
+
+// 12
+let noTelp;
+function setNoTelp(argNoTelp) {
+  noTelp = argNoTelp;
+}
+
+function getNoTelp() {
+  return noTelp;
+}
+console.log(getNoTelp());
+
+// 13
+let jenisKelamin;
+function setJenisKelamin(argJenisKelamin) {
+  jenisKelamin = argJenisKelamin;
+}
+
+function getJenisKelamin() {
+  return jenisKelamin;
+}
+console.log(getJenisKelamin());
+
+// 14
+let agama;
+function setAgama(argAgama) {
+  agama = argAgama;
+}
+
+function getAgama() {
+  return agama;
+}
+console.log(getAgama());
+
+// 15
+let skill;
+function setSkill(argSkill) {
+  skill = argSkill;
+}
+
+function getSkill() {
+  return skill;
+}
+console.log(getSkill());
+
+// 16
+let kewarganegaraan;
+function setKewarganegaraan(argKewarganegaraan) {
+  kewarganegaraan = argKewarganegaraan;
+}
+
+function getKewarganegaraan() {
+  return kewarganegaraan;
+}
+console.log(getKewarganegaraan());
+
+// 17
+let nomorKTP;
+function setNomorKTP(argNomorKTP) {
+  nomorKTP = argNomorKTP;
+}
+
+function getNomorKTP() {
+  return nomorKTP;
+}
+console.log(getNomorKTP());
+
+// 18
+let kendaraan;
+function setKendaraan(argKendaraan) {
+  kendaraan = argKendaraan;
+}
+
+function getKendaraan() {
+  return kendaraan;
+}
+console.log(getKendaraan());
+
+// 19
+let merkKendaraan;
+function setMerkKendaraan(argMerkKendaraan) {
+  merkKendaraan = argMerkKendaraan;
+}
+
+function getMerkKendaraan() {
+  return merkKendaraan;
+}
+console.log(getMerkKendaraan());
+
+// 20
+let makananFavorit;
+function setMakananFavorit(argMakananFavorit) {
+  makananFavorit = argMakananFavorit;
+}
+
+function getMakananFavorit() {
+  return makananFavorit;
+}
+console.log(getMakananFavorit());
